8e92536ac75107afdf7d090db73f29cc76b58e97
iridiscent-hummingbird <0508dima@gmail.com>
2021-03-18 17:50:26 +0100

    Chanche API key

--- lib/api/APIController.dart
+++ lib/api/APIController.dart
@@ -1,13 +1,16 @@
 import 'Location.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
+import 'keys.dart';
 
 class APIController {
-  static String _key = "61448bd85ce69993353c26e7d1cbd6e0";
-
   static Future<Location> fetchData() async {
-    var url = Uri.https('api.openweathermap.org', '/data/2.5/onecall',
-        {'lat': '51.0276', 'lon': '4.4807', 'appid': _key, 'units': 'metric'});
+    var url = Uri.https('api.openweathermap.org', '/data/2.5/onecall', {
+      'lat': '51.0276',
+      'lon': '4.4807',
+      'appid': Keys.weatherKey,
+      'units': 'metric'
+    });
     var response = await http.get(url);
     Location data;
     if (response.statusCode == 200) {
